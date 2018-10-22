d5c1de563738bf75d36c03ec8b8161f2e0a7bf1e
jamesganley <jamesganley96@gmail.com>
2018-10-22 18:24:17 +0100

    First commit

---
+++ inc/nav.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+  <a class="navbar-brand" href="index.php">Cookbook</a>
+  <button class="navbar-toggler"
+  type="button"
+  data-toggle="collapse"
+  data-target="#navbarSupportedContent"
+  aria-controls="navbarSupportedContent"
+  aria-expanded="false"
+  aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="feed.php">Feed</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Profile.php">Profile</a>
+    </li>
+
+
+  </ul>
+  <form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+  include './inc/header.php';
+?>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 fridgeId">
+        <h1 class="fridgeLbl">WHATS IN YOUR FRIDGE</h1>
+          <div class="scrollBar">
+            <p>search</p>
+            <div class="recipeIcon">
+              <p>Proteins</p>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 recipeId">
+        <h1 class="fridgeLbl">HAVE SOME INGREDIENTS? FIND A RECIPE</h1>
+          <div class="scrollBar">
+
+          </div>
+      </div>
+    </div>
+  </div>
+<?php
+  include './inc/footer.php';
+?>
