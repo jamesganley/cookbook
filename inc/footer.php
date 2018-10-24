dbc8b86c2ec0f53da821baa08230bbe1f645a77e
jamesganley <jamesganley96@gmail.com>
2018-10-24 14:41:35 +0100

    2.0 layout
    php'd the new layout

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,57 @@
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-   integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-   crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-   integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-   crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-   integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-   crossorigin="anonymous"></script>
-   
-  </body>
+<!-- Gallery section -->
+<div class="gallery">
+  <div class="gallery-slider owl-carousel">
+    <div class="gs-item set-bg" data-setbg="img/instagram/1.jpg"></div>
+    <div class="gs-item set-bg" data-setbg="img/instagram/2.jpg"></div>
+    <div class="gs-item set-bg" data-setbg="img/instagram/3.jpg"></div>
+    <div class="gs-item set-bg" data-setbg="img/instagram/4.jpg"></div>
+    <div class="gs-item set-bg" data-setbg="img/instagram/5.jpg"></div>
+    <div class="gs-item set-bg" data-setbg="img/instagram/6.jpg"></div>
+  </div>
+</div>
+<!-- Gallery section end -->
+
+
+<!-- Footer section  -->
+<footer class="footer-section set-bg" data-setbg="img/footer-bg.jpg">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="footer-logo">
+          <img src="img/logo.png" alt="">
+        </div>
+        <div class="footer-social">
+          <a href="#"><i class="fa fa-pinterest"></i></a>
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-dribbble"></i></a>
+          <a href="#"><i class="fa fa-behance"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-6 text-lg-right">
+        <ul class="footer-menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Receipies</a></li>
+          <li><a href="#">Reviews</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+</p>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- Footer section end -->
+
+
+
+<!--====== Javascripts & Jquery ======-->
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/main.js"></script>
+</body>
 </html>
