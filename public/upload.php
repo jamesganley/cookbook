80aed65bb44f5d18fee0d9fd10d74f326880e55c
Ashish <blr.ashu@gmail.com>
2018-11-23 14:42:31 +0000

    Images now upload to aws

--- public/upload.php
+++ public/upload.php
@@ -1,27 +1,54 @@
 <?php
-require '../vendor/autoload.php';
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
-$bucket = 'cookbook-strawberry';
-?>
-<html>
-    <head><meta charset="UTF-8"></head>
-    <body>
-        <h1>S3 upload example</h1>
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-    // FIXME: add more validation, e.g. using ext/fileinfo
-    try {
-        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-?>
-        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
-<?php } catch(Exception $e) { ?>
-        <p>Upload error :(</p>
-<?php } } ?>
-        <h2>Upload a file</h2>
-        <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-            <input name="userfile" type="file"><input type="submit" value="Upload">
-        </form>
-    </body>
-</html>
+require __DIR__ . '/../vendor/autoload.php';
+use Aws\S3\S3Client;
+        	use Aws\S3\Exception\S3Exception;
+        	// AWS Info
+        	$bucketName = 'cookbook-strawberry';
+        	$IAM_KEY = 'AKIAIPEP6LNO77KYTAFA';
+        	$IAM_SECRET = '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';
+        	// Connect to AWS
+        	try {
+        		// You may need to change the region. It will say in the URL when the bucket is open
+        		// and on creation.
+        		$s3 = S3Client::factory(
+        			array(
+        				'credentials' => array(
+        					'key' => $IAM_KEY,
+        					'secret' => $IAM_SECRET
+        				),
+        				'version' => 'latest',
+        				'region'  => 'eu-west-1'
+        			)
+        		);
+        	} catch (Exception $e) {
+        		// We use a die, so if this fails. It stops here. Typically this is a REST call so this would
+        		// return a json object.
+        		die("Error:0 " . $e->getMessage());
+        	}
+
+        	// For this, I would generate a unqiue random string for the key name. But you can do whatever.
+        	$keyName = "images/" . basename($_FILES["fileToUpload"]['name']);
+        	$pathInS3 = 'https://s3.eu-west-1.amazonaws.com/' . $bucketName . '/' . $keyName;
+        	// Add it to S3
+        	try {
+        		// Uploaded:
+            $target_dir= $_SERVER["DOCUMENT_ROOT"] . "/static/images/";
+
+        		$file = $target_dir.basename( $_FILES["fileToUpload"]['name']);
+        		$s3->putObject(
+        			array(
+        				'Bucket'=>$bucketName,
+        				'Key' =>  $keyName,
+        				'SourceFile' => $file,
+        				'StorageClass' => 'REDUCED_REDUNDANCY'
+        			)
+        		);
+        	} catch (S3Exception $e) {
+        		die('Error:1' . $e->getMessage());
+        	} catch (Exception $e) {
+        		die('Error:2' . $e->getMessage());
+        	}
+        	echo 'Done uploading ';
+        	// Now that you have it working, I recommend adding some checks on the files.
+        	// Example: Max size, allowed file types, etc.
+        ?>
